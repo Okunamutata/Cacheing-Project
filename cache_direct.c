71ba5d0a6bc08e2c064dda24af0ec0739208b692
Tobenna Okunna <tokunna@sandiego.com>
2019-04-22 09:43:47 -0700

    new direct cache c file

---
+++ cache_direct.c
@@ -0,0 +1,12 @@
+/* 
+ * File:   cache_direct.c
+ * Author: Tobenna Okunna
+           Nolan Bigsono 
+ *
+ * Created on April 22, 2019, 7:33 PM
+ */
+
+#include <cache_direct.h>
+#include <memory.c>
+#include <storage.c>
+
