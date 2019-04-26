295222497f9f7af82fd19df55b390a474eb4a11b
Tobenna Okunna <tokunna@sandiego.com>
2019-04-26 12:56:01 -0700

    -

--- Cacheing-Project/build.bash
+++ Cacheing-Project/build.bash
@@ -12,34 +12,41 @@ gcc -shared -o libtest.so objs/*.o
 
 gcc -std=c99 -Wall -g  \
     -o test1 test1.c \
-    -L/Users/tokunna/GitHub/Cacheing-Project/Cacheing-Project -ltest
+    -L/Users/tokunna/GitHub/Cacheing-Project/Cacheing-Project/test 
+    -ltest
 
 gcc -std=c99 -Wall -g  \
     -o test1a test1a.c \
-    -L/Users/tokunna/GitHub/Cacheing-Project/Cacheing-Project -ltest
+    -L/Users/tokunna/GitHub/Cacheing-Project/Cacheing-Project/test 
+    -ltest
 gcc -std=c99 -Wall -g \
     -o test2 test2.c \
-    -L/Users/tokunna/GitHub/Cacheing-Project/Cacheing-Project -ltest
+    -L/Users/tokunna/GitHub/Cacheing-Project/Cacheing-Project/test 
+    -ltest
 
 gcc -std=c99 -Wall -g \
     -o test3 test3.c \
-    -L/Users/tokunna/GitHub/Cacheing-Project/Cacheing-Project -ltest
+    -L/Users/tokunna/GitHub/Cacheing-Project/Cacheing-Project/test 
+    -ltest
 
 gcc -std=c99 -Wall -g \
     -o test_matrix_sum_16x16 test_matrix_sum_16x16.c \
-    -L/Users/tokunna/GitHub/Cacheing-Project/Cacheing-Project -ltest
+    -L/Users/tokunna/GitHub/Cacheing-Project/Cacheing-Project/test 
+    -ltest
 
 gcc -std=c99 -Wall -g \
     -o test_matrix_sum_8x4 test_matrix_sum_8x4.c \
-    -L/Users/tokunna/GitHub/Cacheing-Project/Cacheing-Project -ltest
+    -L/Users/tokunna/GitHub/Cacheing-Project/Cacheing-Project/test 
+    -ltest
 
 gcc -std=c99 -Wall -g \
     -o test_transpose_8x8 test_transpose_8x8.c \
-    -L./Users/tokunna/GitHub/Cacheing-Project/Cacheing-Project -ltest
+    -L./Users/tokunna/GitHub/Cacheing-Project/Cacheing-Project/test -ltest
 
 gcc -std=c99 -Wall -g \
     -o test_transpose_8x8_improved test_transpose_8x8_improved.c \
-    -L/Users/tokunna/GitHub/Cacheing-Project/Cacheing-Project -ltest
+    -L/Users/tokunna/GitHub/Cacheing-Project/Cacheing-Project/test 
+    -ltest
 
 
 
